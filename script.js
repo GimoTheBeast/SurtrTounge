57284675cb8a6a21827d90e3e4e999a3769d4a6f
Devlyn Guarino <ganorith@gmail.com>
2022-04-01 12:48:23 -0400

    bug fixes

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ let toSurtr = function() {
 	for(i = 0; i < engArray.length; i++) {
 		let letterCheck = /^[a-zA-Z]+$/.test(engArray[i])
 		if (!letterCheck){
-			outputString.push(engArray[i]) 
+			outputString.push(engArray[i])
 		}
 		else{
 			let isUppercase = engArray[i] == engArray[i].toUpperCase();
@@ -19,34 +19,27 @@ let toSurtr = function() {
 			else{
 				outputString.push(sSpeak.toLowerCase());
 			}
-			srt.value = outputString.join("")
 		}
+		srt.value = outputString.join("")
 	}
 }
 let indexSearch = function(array, engArray, i, sArray){
-	//console.log(array + " " + engArray + " " + i);
 	if(array.length == 0){
 		console.error("Entire Array Emptied")
 		return;
 	}
-	//console.log(array);
 	let halfIndex = Math.floor(array.length / 2)
-	//console.log(halfIndex + " " + array[halfIndex])
 	if (array[halfIndex] == engArray[i].toUpperCase()){
-		console.log("exact " + engArray[i].toUpperCase() + " " + array[halfIndex])
 		return sArray[halfIndex]
 	}
 	else if(array[halfIndex] < engArray[i].toUpperCase()) {
 		contArray = array.slice(halfIndex, array.length)
 		contsArray = sArray.slice(halfIndex, array.length)
-		//console.log("after " + contArray)
 	}
 	else {
 		contArray = array.slice(0, halfIndex)
 		contsArray = sArray.slice(0, halfIndex)
-		//console.log("before " + contArray)
 	}
-	console.log(sArray)
 	return indexSearch(contArray, engArray, i, contsArray);
 }
 let toEnglish = function() {
@@ -64,10 +57,8 @@ let toEnglish = function() {
 			console.log(i)
 			let isUppercase = srtArray[i] == srtArray[i].toUpperCase();
 			for(letter = 0;letter < surtrSpeak.length; letter++){
-				console.log(surtrSpeak[letter] + " " + srtArray[i])
 				if(surtrSpeak[letter] == srtArray[i].toUpperCase()){
 					eSpeak = english[letter]
-					console.log(letter)
 				}
 			}
 			if(isUppercase){
